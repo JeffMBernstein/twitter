d8755ef20175bbf23099ed21081d936a90f1b11e
Ahmed O <aomra015@gmail.com>
2013-07-29 21:44:11 -0400

    add validation tests and code

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "tweet can get user's id" do
+    tweet = tweets(:one)
+    assert_equal tweet.user.id, users(:one).id
+  end
+
+  test "tweet can't be blank" do
+    tweet = Tweet.new(content: "")
+    tweet.save
+
+    assert tweet.errors.any?, 'Blank tweet can be created'
+  end
+
+  test "tweet can't be longer than 140 characters" do
+    tweet = Tweet.new(content: "This is a huge line of text because I like to blog instead of tweeting. Tweeting is dumb, seriously... how can one say anything useful in 140 characters?")
+    tweet.save
+
+    assert tweet.errors.any?, 'Long tweet can be created'
+  end
 end
